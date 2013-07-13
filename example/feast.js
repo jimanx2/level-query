49b6c93572b0e8b8272334131496748fa12f0901
James Halliday <mail@substack.net>
2013-07-13 01:40:50 -0700

    simplify the example by taking the id from the data

--- example/feast.js
+++ example/feast.js
@@ -1,34 +1,25 @@
 var http = require('http');
+var concat = require('concat-stream');
+
 var levelup = require('levelup');
 var sublevel = require('level-sublevel');
-
-var crypto = require('crypto');
-var concat = require('concat-stream');
-var through = require('through');
-var duplexer = require('duplexer');
 
 var db = sublevel(levelup(__dirname + '/db', { encoding: 'json' }));
 var feast = require('../')(db);
 
-var stringify = require('JSONStream').stringify;
-
 var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        req.pipe(putHash()).pipe(concat(function (hash) {
-            res.end(hash + '\n');
-        }));
-    }
-    else if (req.method === 'GET') {
-        var s = feast(req.url);
-        s.on('error', function (err) {
-            res.statusCode = err.code;
-            if (!res._headerSent) {
-                res.setHeader('content-type', 'text/plain');
-            }
-            res.end(err + '\n');
-        });
+    if (req.method === 'GET') {
         res.setHeader('content-type', 'application/json');
+        
+        var s = feast(req.url);
+        s.on('error', function (err) { res.end(err + '\n') });
         s.pipe(res);
+    }
+    else if (req.method === 'POST') {
+        req.pipe(concat(function (body) {
+            db.put(req.url.slice(1), JSON.parse(body));
+            res.end('ok\n');
+        }));
     }
     else {
         res.statusCode = 404;
@@ -36,23 +27,3 @@ var server = http.createServer(function (req, res) {
     }
 });
 server.listen(4000);
-
-function putHash () {
-    var input = through();
-    var sha1 = crypto.createHash('sha1', { encoding: 'hex' });
-    
-    input.pipe(concat(function (body) {
-        try { var obj = JSON.parse(body) }
-        catch (err) {
-            return dup.emit('error', err);
-        }
-        
-        sha1.pipe(concat(function (hash) {
-            db.put(hash, obj);
-        }));
-        sha1.end(JSON.stringify(obj));
-    }));
-    
-    var dup = duplexer(input, sha1);
-    return dup;
-}
